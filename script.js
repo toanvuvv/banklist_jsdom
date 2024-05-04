8c2ccb4d7e58681b00d65c4aa7b0dfc6cdc00b9b
toanvuvv <toan.vd205173@sis.hust.edu.vn>
2024-05-04 10:12:43 +0700

    scroll

--- script.js
+++ script.js
@@ -29,3 +29,48 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// // Insert Adjacent HTML
+// const message = document.createElement('div');
+// message.innerHTML =
+//   'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>';
+// const header = document.querySelector('.header');
+// message.classList.add('cookie-message');
+// header.append(message);
+
+// //Delete Elements
+// document
+//   .querySelector('.btn--close-cookie')
+//   .addEventListener('click', function () {
+//     message.remove();
+//   });
+// // Styles
+// message.style.backgroundColor = '#37083d';
+// console.log(getComputedStyle(message).color);
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+  console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+  console.log('height/width viewport', document.documentElement.clientHeight, document.documentElement.clientWidth);
+  // scrolling
+  window.scrollTo({
+    left: s1coords.left+ window.pageXOffset,
+    top: s1coords.top+ window.pageYOffset,
+    behavior: 'smooth',
+  });
+});
+
+const h1 = document.querySelector('h1');
+const alertH1 = function (e) {
+  alert('addEventListener: Great! You are reading the heading :D');
+}
+h1.addEventListener('mouseenter',alertH1 );
